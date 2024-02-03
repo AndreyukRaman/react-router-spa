de5757edce83db849a618e03e7c93724a437f468
Raman Andreyuk <romaandreyuk@gmail.com>
2024-02-03 16:49:30 +0100

    fixed prop name in countryItem

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -8,16 +8,16 @@ function CountryList({ cities, isLoading }) {
 
   if (!cities.length)
     return <Message message="Add your fist city by clicking on the map" />;
-
   const countries = cities.reduce((arr, city) => {
     if (!arr.map((el) => el.country).includes(city.country))
       return [...arr, { country: city.country, emoji: city.emoji }];
     else return arr;
   }, []);
+
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem city={country} key={country.id} />
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
